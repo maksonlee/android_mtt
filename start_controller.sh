580b0ef3f57e10916dd5fcd4eb87f09160088c98
Makson Lee <cdlee123@gmail.com>
2023-07-23 08:55:49 +0800

    Initial commit

---
+++ start_controller.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/android_mtt/mtt --log_file /var/log/mtt/mtt.log start --operation_mode=ON_PREMISE --tag prod_1.45.000.arima
---
+++ start_worker.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/android_mtt/mtt --log_file /var/log/mtt/mtt.log start --control_server_url=http://ats-controller:8000 --operation_mode=ON_PREMISE --tag prod_1.45.000.arima
